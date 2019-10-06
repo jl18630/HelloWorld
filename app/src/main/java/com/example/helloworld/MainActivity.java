d57584660db4398a143f33d82a7f32cc65d30a52
jl18630 <k.lee824@gmail.com>
2019-10-05 20:14:14 -0400

    Test Push to AndroidTest repo.

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Button buttonRegister = findViewById(R.id.buttonRegister);
+
+        //TextView textViewFirstName = findViewById(R.id.firstNameEditText);
+        //TextView textViewLastName = findViewById(R.id.lastNameEditText);
+        //TextView textViewEmail = findViewById(R.id.emaiEditText);
+    }
+
+    public void onButtonClickRegister (View view) {
+        TextView textViewFirstName = findViewById(R.id.textFirstName);
+        TextView textViewLastName = findViewById(R.id.textViewLastName);
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+
+        //textViewFirstName.setText("Hello, User");
+
+        EditText editTextFirstName = findViewById(R.id.firstNameEditText);
+        EditText editTextLastName = findViewById(R.id.lastNameEditText);
+        EditText editTextEmail = findViewById(R.id.emailEditText);
+
+        textViewFirstName.setText(editTextFirstName.getText().toString());
+        textViewLastName.setText(editTextLastName.getText().toString());
+        textViewEmail.setText(editTextEmail.getText().toString());
+    }
+}
